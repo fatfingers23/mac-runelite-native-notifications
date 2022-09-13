d25e6225f76aadda36ee6d8493c97d8069edec8e
Bailey Townsend <baileytownsend2323@gmail.com>
2022-09-12 20:44:08 -0500

    Allow plugin to handle turning on and off tray notifications

--- src/main/java/com/NativeMacNotifications/NativeMacNotificationsPlugin.java
+++ src/main/java/com/NativeMacNotifications/NativeMacNotificationsPlugin.java
@@ -1,7 +1,9 @@
 package com.NativeMacNotifications;
 
 import com.google.common.base.Strings;
+
 import javax.inject.Inject;
+
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.Player;
@@ -18,100 +20,88 @@ import net.runelite.client.util.OSType;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Native Mac Notifications"
+        name = "Native Mac Notifications"
 )
-public class NativeMacNotificationsPlugin extends Plugin
-{
-	@Inject
-	private Client client;
+public class NativeMacNotificationsPlugin extends Plugin {
+    @Inject
+    private Client client;
 
-	@Inject
-	private PluginManager pluginManager;
+    @Inject
+    private PluginManager pluginManager;
 
-	@Inject
-	private RuneLiteConfig runeLiteConfig;
+    @Inject
+    private RuneLiteConfig runeLiteConfig;
 
-	@Inject
-	private ClientUI clientUI;
+    @Inject
+    private ClientUI clientUI;
 
-	@Inject
-	private ConfigManager configManager;
+    @Inject
+    private ConfigManager configManager;
 
-	private Boolean resetTrayNotificationsAtShutDown;
+    private Boolean resetTrayNotificationsAtShutDown = false;
 
-	@Subscribe
-	public void onNotificationFired(NotificationFired notificationFired)
-	{
-		try
-		{
-			if (!runeLiteConfig.sendNotificationsWhenFocused() && clientUI.isFocused())
-			{
-				return;
-			}
+    @Subscribe
+    public void onNotificationFired(NotificationFired notificationFired) {
+        try {
+            if (OSType.getOSType() != OSType.MacOS) {
+                return;
+            }
 
-			Notification.notify(buildTitle(), notificationFired.getMessage());
-		}
-		catch (Exception e)
-		{
-			stopPlugin();
-		}
-	}
+            if (!runeLiteConfig.sendNotificationsWhenFocused() && clientUI.isFocused()) {
+                return;
+            }
 
-	@Override
-	protected void startUp()
-	{
-		if (OSType.getOSType() != OSType.MacOS)
-		{
-			stopPlugin();
-		}
-		else
-		{
-			if (runeLiteConfig.enableTrayNotifications())
-			{
-				resetTrayNotificationsAtShutDown = true;
-				configManager.setConfiguration(RuneLiteConfig.GROUP_NAME, "notificationTray", false);
-			}
-		}
-	}
+            Notification.notify(buildTitle(), notificationFired.getMessage());
+        } catch (Exception e) {
+            stopPlugin();
+        }
+    }
 
-	@Override
-	protected void shutDown()
-	{
-		if (resetTrayNotificationsAtShutDown)
-		{
-			configManager.setConfiguration(RuneLiteConfig.GROUP_NAME, "notificationTray", true);
-		}
-	}
+    @Override
+    protected void startUp() {
+        if (OSType.getOSType() == OSType.MacOS) {
+            if (runeLiteConfig.enableTrayNotifications()) {
+                resetTrayNotificationsAtShutDown = true;
+                this.setNotificationTray(false);
+            }
+        }
+    }
 
-	private void stopPlugin()
-	{
-		try
-		{
-			pluginManager.setPluginEnabled(this, false);
-			pluginManager.stopPlugin(this);
-		}
-		catch (PluginInstantiationException ex)
-		{
-			log.error("error stopping plugin", ex);
-		}
-	}
+    @Override
+    protected void shutDown() {
+        if (resetTrayNotificationsAtShutDown) {
+            this.setNotificationTray(true);
+        }
+    }
 
-	private String buildTitle()
-	{
-		String notificationTitle = "RuneLite";
+    private void stopPlugin() {
+        try {
+            pluginManager.setPluginEnabled(this, false);
+            pluginManager.stopPlugin(this);
+        } catch (PluginInstantiationException ex) {
+            log.error("error stopping plugin", ex);
+        }
+    }
 
-		Player player = client.getLocalPlayer();
-		if (player == null)
-		{
-			return notificationTitle;
-		}
+    private String buildTitle() {
+        String notificationTitle = "RuneLite";
 
-		String name = player.getName();
-		if (Strings.isNullOrEmpty(name))
-		{
-			return notificationTitle;
-		}
+        Player player = client.getLocalPlayer();
+        if (player == null) {
+            return notificationTitle;
+        }
 
-		return notificationTitle + " - " + name;
-	}
+        String name = player.getName();
+        if (Strings.isNullOrEmpty(name)) {
+            return notificationTitle;
+        }
+
+        return notificationTitle + " - " + name;
+    }
+
+    private void setNotificationTray(boolean enable) {
+        if (OSType.getOSType() == OSType.MacOS) {
+            configManager.setConfiguration(RuneLiteConfig.GROUP_NAME, "notificationTray", enable);
+        }
+    }
 }
